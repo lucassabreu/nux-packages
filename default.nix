ad1a34e0bcca99f21b6d0a154f192ac525ca77be
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2025-09-30 12:10:36 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  clockify-cli = pkgs.callPackage ./pkgs/clockify-cli { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
